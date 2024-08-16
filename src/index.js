3008233580141824121987c9069b13876bf229f7
Samuel Mburu <sammxuser@gmail.com>
2024-08-16 09:49:50 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import logo from './logo.jpeg';
+
+function component() {
+  const element = document.createElement('div');
+
+  //   lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  //   Add the log to the div
+  const myLogo = new Image();
+  myLogo.src = logo;
+
+  element.appendChild(myLogo);
+
+  return element;
+}
+
+document.body.appendChild(component());
