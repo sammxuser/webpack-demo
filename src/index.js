1a0449df43105fcafe9d11efddbaef625e72a033
Samuel Mburu <sammxuser@gmail.com>
2024-08-16 19:37:34 +0300

    Initial commit(Unbundle)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import _ from 'lodash';
-import './style.css';
-import logo from './logo.jpeg';
+// import _ from 'lodash';
+// import './style.css';
+// import logo from './logo.jpeg';
 
 function component() {
   const element = document.createElement('div');
 
   //   lodash, now imported by this script
+  //   Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  //   element.classList.add('hello');
 
   //   Add the log to the div
-  const myLogo = new Image();
-  myLogo.src = logo;
+  //   const myLogo = new Image();
+  //   myLogo.src = logo;
 
-  element.appendChild(myLogo);
+  //   element.appendChild(myLogo);
 
   return element;
 }
